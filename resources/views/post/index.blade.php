350d12ed01173ff1cefc3f36b074084186925e24
Robert <rcoffie22@yahoo.com>
2019-10-04 18:54:15 +0000

    ''

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.main')
+
+
+@section('content')
+
+<ul class="flex p-4">
+        <li class="mr-6">
+          <a class="text-blue-500 hover:text-blue-800" href="#">Active</a>
+        </li>
+        <li class="mr-6">
+          <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+        </li>
+        <li class="mr-6">
+          <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+        </li>
+        <li class="mr-6">
+          <a class="text-gray-400 cursor-not-allowed" href="#">Disabled</a>
+        </li>
+ </ul>
+
+<div class="container mx-auto px-4">
+
+<div class="flex ">
+
+     <div class="w-2/3 bg-gray-400 h-auto m1 "> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas exercitationem ex itaque, ratione dolore voluptatibus maiores vel est asperiores, obcaecati reiciendis possimus inventore labore necessitatibus consequatur debitis dolores beatae  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga unde voluptates ex vitae accusamus. Fugiat praesentium iste beatae temporibus nisi quaerat culpa eius corporis ducimus! Ipsum non dignissimos asperiores doloremque.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio accusamus sed eum maiores repellat quaerat nobis nostrum quasi blanditiis, dignissimos in optio repellendus dicta perspiciatis dolores sequi, recusandae cupiditate illo. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, eveniet! Labore nisi, eveniet molestiae impedit, voluptatem voluptates voluptas esse libero ea eaque sapiente quidem explicabo nulla debitis praesentium aliquam? Perferendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. A nesciunt hic itaque minima dignissimos sit consequuntur magni in, quos harum unde ratione repellat corporis? Impedit minima non asperiores assumenda vero! </div>
+
+
+    <div class="w-1/3 bg-gray-500 h-auto m-1"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis praesentium repudiandae expedita quasi necessitatibus commodi corporis illum rem, odit id fuga iure quisquam magnam explicabo numquam vitae doloribus voluptatum quia!</div>
+
+ </div>
+
+
+
+ <div class="flex flex-wrap bg-gray-200">
+        <div class="w-1/3 p-2">
+          <div class="text-gray-700 text-center bg-gray-400 p-2"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero aspernatur nulla earum quo rerum sed ex quibusdam eum ad! Dignissimos nihil repellat sequi aperiam! Nobis quae saepe quia quisquam culpa! </div>
+        </div>
+        <div class="w-1/3 p-2">
+          <div class="text-gray-700 text-center bg-gray-400 p-2"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta quaerat, nihil ea distinctio voluptas minima. Vitae cum iure quo. Ullam quidem qui aliquid accusamus voluptates provident, tempore ipsum assumenda eum. </div>
+        </div>
+        <div class="w-1/3 p-2">
+          <div class="text-gray-700 text-center bg-gray-400 p-2"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio voluptatem praesentium tempora vel, maiores quis amet eius voluptates dolorem corrupti totam, mollitia odio consectetur? Suscipit aspernatur ad praesentium nihil provident. </div>
+        </div>
+      </div>
+
+      <div class="w-1/3 p-2">
+        <div class="text-gray-700 text-center bg-gray-400 p-2"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio voluptatem praesentium tempora vel, maiores quis amet eius voluptates dolorem corrupti totam, mollitia odio consectetur? Suscipit aspernatur ad praesentium nihil provident. </div>
+      </div>
+    </div>
+
+
+
+</div>
+
+
+
+<div class="container mx-auto px-4">
+
+<h3 class="text-xl text-red-400 m-2"> Lastest News </h3>
+
+<div class="flex p-2 ">
+
+        <div class="w-1/3 bg-gray-400 h-auto p-1 m-1">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum ea explicabo repudiandae quidem iure. Illo exercitationem sunt pariatur quam, rem nostrum aut quidem officiis illum consequatur, numquam minima quo quaerat.</div>
+
+       <div class="w-1/3 bg-gray-500 h-auto p-1 m-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aperiam sed facilis itaque eum, nulla repellat. Molestias incidunt, cupiditate tempora, ipsum quaerat explicabo totam eaque et quidem, magni ea ipsam.</div>
+
+       <div class="w-1/3 bg-gray-300 h-auto p-1 m-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dicta eius, accusamus sunt eum nemo quisquam laboriosam sint vero illum, quae libero ut dolores quo sed facere corrupti, rem totam.</div>
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+@endsection
+
+
+
