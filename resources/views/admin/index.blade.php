d3a99c43709e5ca0b427abfa6158170fa93627a5
Robert <rcoffie22@yahoo.com>
2019-10-11 15:19:20 +0000

    ''

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.main')
+<ul class="flex p-4 bg-blue-900">
+    <li class="mr-6">
+      <a class="text-blue-500 hover:text-blue-800" href="#">Active</a>
+    </li>
+    <li class="mr-6">
+      <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+    </li>
+    <li class="mr-6">
+      <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+    </li>
+    <li class="mr-6">
+      <a class="text-gray-400 cursor-not-allowed" href="#">Disabled</a>
+    </li>
+</ul>
+
+<!---------- BOdy ------------------>
+
+<div class="flex ">
+
+    <div class="w-1/6 bg-gray-400 h-auto p-2 ">
+
+
+
+          <ol class="list-default m-auto mx-8">
+
+            <li class="">  <a class="text-blue-500 hover:text-blue-800" href="#">Active</a> </li>
+            <li>  <a class="text-blue-500 hover:text-blue-800" href="#">Active</a> </li>
+            <li>  <a class="text-blue-500 hover:text-blue-800" href="#">Active</a> </li>
+            <li>  <a class="text-blue-500 hover:text-blue-800" href="#">Active</a> </li>
+
+
+          </ol>
+
+
+
+    </div>
+
+
+   <div class="w-5/6 bg-gray-500 h-auto p-2"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis praesentium repudiandae expedita quasi necessitatibus commodi corporis illum rem, odit id fuga iure quisquam magnam explicabo numquam vitae doloribus voluptatum quia!</div>
+
+</div>
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('post', 'PostController');
 
+Route::get('/admin', function () {
+    return view('admin.index');
+});
